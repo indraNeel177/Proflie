1d78fb490ce831e82c3a0359972525a6d99eb2ac
indraNeel117 <indraneel@zestiot.io>
2021-02-14 22:27:35 +0530

    created call-based  views and modified tested results

--- Proflie/Proflie/urls.py
+++ Proflie/Proflie/urls.py
@@ -1,23 +1,14 @@
-"""Proflie URL Configuration
+from django.urls import path, include
+from proflieApp.views import message_list, Message_edit, MessageApiView, MessageDetails, Message_list, message_edit
+from rest_framework.urlpatterns import format_suffix_patterns
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path
 
 urlpatterns = [
-    path('users/admin/', admin.site.urls),
+    path('sample/', Message_list),
+    path('sample1/<int:pk>', Message_edit),
+    path('apiview/', message_list),
+    path('apiview1/<int:pk>', message_edit),
+    path('class/', MessageApiView.as_view()),
+    path('class1/<int:id>', MessageDetails.as_view()),
 ]
-from rest_framework.urlpatterns import format_suffix_patterns
-from snippets import views
+urlpatterns = format_suffix_patterns(urlpatterns)
--- Proflie/proflieApp/views.py
+++ Proflie/proflieApp/views.py
@@ -1,38 +1,135 @@
-from django.shortcuts import render
-from models import Snippet
-from serializers import SnippetSerializer
-from django.http import Http404
-from rest_framework.views import APIView
+from django.http import HttpResponse, JsonResponse
+from rest_framework.parsers import JSONParser
+from .models import messgae
+from .serializers import messageSerializer
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.views import APIView
 
 
-class SnippetDetail(APIView):
-    """
-    Retrieve, update or delete a snippet instance.
-    """
-    def get_object(self, pk):
-        try:
-            return Snippet.objects.get(pk=pk)
-        except Snippet.DoesNotExist:
-            raise Http404
+class MessageApiView(APIView):
 
-    def get(self, request, pk, format=None):
-        snippet = self.get_object(pk)
-        serializer = SnippetSerializer(snippet)
+    def get(self, request):
+        message = messgae.objects.all()
+        serializer = messageSerializer(message, many=type)
         return Response(serializer.data)
 
-    def put(self, request, pk, format=None):
-        snippet = self.get_object(pk)
-        serializer = SnippetSerializer(snippet, data=request.data)
+    def post(self, request):
+        serializer = messageSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class MessageDetails(APIView):
+
+    def get_object(self, id):
+        try:
+            message1 = messgae.objects.get(id=id)
+            return message1
+        except message1.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+    def get(self, request, id):
+        message1 = self.get_object(id)
+        serializer = messageSerializer(message1)
+        return Response(serializer.data)
+
+    def put(self, request, id):
+        message1 = self.get_object(id)
+        serializer = messageSerializer(message1, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def delete(self, request, pk, format=None):
-        snippet = self.get_object(pk)
-        snippet.delete()
+    def delete(self, request, id):
+        message1 = self.get_object(id)
+        message1.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+@api_view(['GET', 'POST'])
+def message_list(request):
+    if request.method == "GET":
+        message = messgae.objects.all()
+        serializer = messageSerializer(message, many=type)
+        return Response(serializer.data)
+
+    elif request.method == "POST":
+        serializer = messageSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def message_edit(request, pk):
+    try:
+        message1 = messgae.objects.get(pk=pk)
+    except message1.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == "GET":
+        serializer = messageSerializer(message1)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = messageSerializer(message1, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        message1.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+@csrf_exempt
+def Message_list(request):
+    if request.method == "GET":
+        message = messgae.objects.all()
+        serializer = messageSerializer(message, many=type)
+        return JsonResponse(serializer.data, safe=False)
+
+    elif request.method == "POST":
+        data = JSONParser().parse(request)
+        serializer = messageSerializer(data=data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+
+
+@csrf_exempt
+def Message_edit(request, pk):
+    try:
+        message1 = messgae.objects.get(pk=pk)
+    except message1.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == "GET":
+        serializer = messageSerializer(message1)
+        return JsonResponse(serializer.data)
+
+    elif request.method == "PUT":
+        data = JSONParser().parse(request)
+        serializer = messageSerializer(message1, data=data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+
+    elif request.method == 'DELETE':
+        message1.delete()
+        return HttpResponse(status=204)
 
 # Create your views here.
